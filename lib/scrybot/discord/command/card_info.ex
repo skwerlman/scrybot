32debe8dc3fc578816d2a69afc783aab7ed64b5d
skwerlman <skwerlman@users.noreply.github.com>
2018-07-03 20:56:42 -0400

    enormous CardInfo rewrite

--- lib/scrybot/discord/command/card_info.ex
+++ lib/scrybot/discord/command/card_info.ex
@@ -5,110 +5,98 @@ defmodule Scrybot.Discord.Command.CardInfo do
   alias Nostrum.Struct.Embed
   alias Nostrum.Struct.Message
   alias Nostrum.Struct.User
+  alias Scrybot.Discord.Colors
   alias Scrybot.Discord.Emoji
 
+  @doc false
   def init do
-    Logger.info("CardInfo command set loaded")
+    Logger.info("CardInfoNew command set loaded")
   end
 
+  @doc false
   def allow_bots?, do: false
 
-  def do_command(%Message{author: %User{bot: bot}, content: "[[quack]]"} = message)
-      when bot in [false, nil] do
-    Api.create_message(message.channel_id, content: ":duck:")
+  @doc """
+  Scan the message for patterns like [[card name]] and look up info
+  about each of those cards.
+  """
+  def do_command(%Message{author: %User{bot: bot}} = message) when bot in [false, nil] do
+    ~r/(?:\[\[(.*?)\]\])/
+    |> Regex.scan(message.content)
+    # Regex.scan returns results as a list of lists
+    # We only care about the second capture, so we strip off the rest
+    |> Stream.map(fn [_ | [x | _]] -> x end)
+    # Its possible for the above filter to return an
+    # empty list, so we remove it here
+    |> Stream.reject(fn x -> x == [] end)
+    # We only want to handle a given card once, so we filter for unique cards
+    |> Stream.uniq()
+    # Finally, we run handle_card on each of the cards
+    # This is synchronous to avoid split responses being out-of-order
+    |> Enum.each(fn x -> handle_card(x, message) end)
+
+    ~r/(?:\[e\[(.*?)\]\])/
+    |> Regex.scan(message.content)
+    |> Stream.map(fn [_ | [x | _]] -> x end)
+    |> Stream.reject(fn x -> x == [] end)
+    |> Stream.uniq()
+    |> Enum.each(fn x -> handle_exact_card(x, message) end)
   end
 
-  def do_command(%Message{author: %User{bot: bot}} = message) when bot in [false, nil] do
-    cap =
-      Regex.named_captures(
-        ~r/.*\[\[(?<card_name>.*)\]\].*/,
-        message.content
-      )
+  defp handle_exact_card(card_name, ctx) do
+    card_info =
+      card_name
+      |> get_exact_card_info()
 
-    {embed, info} =
-      case cap do
-        nil ->
-          {nil, nil}
-
-        _ ->
-          cap["card_name"]
-          |> get_card_info()
-          |> case do
-            {:ok, %{body: info}} ->
-              {build_card_embed(info), info}
-
-            {:error, reason} ->
-              notify_error(reason, message.channel_id)
-              {nil, nil}
+    case card_info do
+      {:ok, info} ->
+        rulings =
+          case get_rulings(info.body["id"]) do
+            {:ok, %{body: resp}} -> resp["data"]
+            {:error, reason} -> notify_error(reason, ctx.channel_id)
           end
-      end
 
-    if embed do
-      Logger.warn("TRYING TO POST")
+        return_card(info, rulings, ctx)
 
-      case Nostrum.Api.create_message(message.channel_id, embed: embed) do
-        {:ok, _} ->
-          :ok
+      {:error, _, "ambiguous"} ->
+        card_name
+        |> get_alternate_cards()
+        |> return_alternate_cards(card_name, ctx)
 
-        error ->
-          Logger.error(inspect(error))
+      {:error, message, _} ->
+        return_error(message, ctx)
 
-          notify_error!(
-            %Embed{
-              description: "```\n#{inspect(error)}\n```",
-              title: "Nostrum Error!",
-              color: 0xE74C3C
-            },
-            message.channel_id
-          )
-      end
-
-      rulings =
-        case get_rulings(info["id"]) do
-          {:ok, %{body: resp}} -> resp["data"]
-          {:error, reason} -> notify_error(reason, message.channel_id)
-        end
-
-      ruling_embed =
-        %Embed{}
-        |> put_rulings(rulings)
-        |> Embed.put_footer(
-          # case Enum.random(1..10000) do
-          #   1..9999 -> "data sourced from Scryfall"
-          #   10000 -> "data forcefully ripped from the cold, dead hands of Scryfall"
-          # end,
-          "data sourced from Scryfall",
-          "https://cdn.discordapp.com/app-icons/268547439714238465/f13c4408ead703ef3940bc7e21b91e2b.png"
-        )
-
-      case Api.create_message(message.channel_id, embed: ruling_embed) do
-        {:ok, _} ->
-          :ok
-
-        error ->
-          Logger.error(inspect(error))
-
-          notify_error!(
-            %Embed{
-              description: "```\n#{inspect(error)}\n```",
-              title: "Nostrum Error!",
-              color: 0xE74C3C
-            },
-            message.channel_id
-          )
-      end
+      {:error, message} ->
+        return_error(message, ctx)
     end
   end
 
-  defp build_card_embed(card_info) do
-    %Embed{}
-    |> Embed.put_title(md_escape(card_info["name"]))
-    |> Embed.put_url(card_info["scryfall_uri"])
-    |> Embed.put_description(
-      Emoji.emojify(card_info["mana_cost"] <> "\n" <> format_description(card_info))
-    )
-    |> put_legalities(card_info["legalities"])
-    |> Embed.put_image(card_info["image_uris"]["art_crop"])
+  defp handle_card(card_name, ctx) do
+    card_info =
+      card_name
+      |> get_card_info()
+
+    case card_info do
+      {:ok, info} ->
+        rulings =
+          case get_rulings(info.body["id"]) do
+            {:ok, %{body: resp}} -> resp["data"]
+            {:error, reason} -> notify_error(reason, ctx.channel_id)
+          end
+
+        return_card(info, rulings, ctx)
+
+      {:error, _, "ambiguous"} ->
+        card_name
+        |> get_alternate_cards()
+        |> return_alternate_cards(card_name, ctx)
+
+      {:error, message, _} ->
+        return_error(message, ctx)
+
+      {:error, message} ->
+        return_error(message, ctx)
+    end
   end
 
   defp put_rulings(embed, []), do: embed
@@ -120,45 +108,42 @@ defmodule Scrybot.Discord.Command.CardInfo do
   end
 
   defp put_legalities(embed, legalities) do
-    legal_formats =
-      legalities
-      |> Map.to_list()
-      |> Enum.filter(fn {_, v} -> v == "legal" end)
-      |> Enum.map(fn {k, _} -> "- #{k}" end)
-
-    legal_group_1 =
-      legal_formats
-      |> Enum.take_every(2)
-      |> Enum.join("\n")
-
-    legal_group_2 =
-      legal_formats
-      |> Enum.drop(1)
-      |> Enum.take_every(2)
-      |> Enum.join("\n")
-
-    case {legal_group_1, legal_group_2} do
-      {"", ""} ->
+    case legalities do
+      nil ->
         embed
 
-      {_, ""} ->
-        embed
-        |> Embed.put_field("Legal in", legal_group_1, true)
+      _ ->
+        legal_formats =
+          legalities
+          |> Map.to_list()
+          |> Enum.filter(fn {_, v} -> v == "legal" end)
+          |> Enum.map(fn {k, _} -> "- #{k}" end)
 
-      {_, _} ->
-        embed
-        |> Embed.put_field("Legal in", legal_group_1, true)
-        |> Embed.put_field("\u200D", legal_group_2, true)
+        legal_group_1 =
+          legal_formats
+          |> Enum.take_every(2)
+          |> Enum.join("\n")
+
+        legal_group_2 =
+          legal_formats
+          |> Enum.drop(1)
+          |> Enum.take_every(2)
+          |> Enum.join("\n")
+
+        case {legal_group_1, legal_group_2} do
+          {"", ""} ->
+            embed
+
+          {_, ""} ->
+            embed
+            |> Embed.put_field("Legal in", legal_group_1, true)
+
+          {_, _} ->
+            embed
+            |> Embed.put_field("Legal in", legal_group_1, true)
+            |> Embed.put_field("\u200D", legal_group_2, true)
+        end
     end
-  end
-
-  defp md_escape(text) do
-    text
-    |> String.replace("_", "\\_")
-    |> String.replace("~", "\\~")
-    |> String.replace("*", "\\*")
-    |> String.replace("`", "\\`")
-    |> String.replace(">", "\\>")
   end
 
   defp format_description(card) do
@@ -191,6 +176,129 @@ defmodule Scrybot.Discord.Command.CardInfo do
     end
   end
 
+  defp md_escape(text) do
+    text
+    |> String.replace("_", "\\_")
+    |> String.replace("~", "\\~")
+    |> String.replace("*", "\\*")
+    |> String.replace("`", "\\`")
+    |> String.replace(">", "\\>")
+  end
+
+  defp fits_limits?(_info, rulings) do
+    # TODO make this smarter
+    case length(rulings) do
+      x when x <= 5 -> true
+      _ -> false
+    end
+  end
+
+  defp return_card(%{body: %{"layout" => layout} = body}, rulings, ctx)
+       when layout in ["split", "flip", "transform", "double_faced_token"] do
+    [last_face | faces] = body["card_faces"] |> Enum.reverse()
+
+    for face <- faces |> Enum.reverse() do
+      Map.merge(body, face)
+    end
+    |> Enum.each(fn x ->
+      return_card(x, [], ctx)
+    end)
+
+    case last_face do
+      nil ->
+        :ok
+
+      face ->
+        return_card(face, rulings, ctx)
+    end
+  end
+
+  defp return_card(card_info, rulings, ctx) do
+    info = card_info.body
+
+    embeds =
+      case fits_limits?(info, rulings) do
+        true ->
+          [
+            %Embed{}
+            |> Embed.put_title(md_escape(info["name"]))
+            |> Embed.put_url(info["scryfall_uri"])
+            |> Embed.put_description(
+              Emoji.emojify(info["mana_cost"] <> "\n" <> format_description(info))
+            )
+            |> put_legalities(info["legalities"])
+            |> Embed.put_image(info["image_uris"]["small"])
+            |> put_rulings(rulings)
+            |> Embed.put_footer(
+              case Enum.random(1..5000) do
+                x when x in 1..4999 -> "data sourced from Scryfall"
+                5000 -> "data forcefully ripped from the cold, dead hands of Scryfall"
+              end,
+              "https://cdn.discordapp.com/app-icons/268547439714238465/f13c4408ead703ef3940bc7e21b91e2b.png"
+            )
+          ]
+
+        false ->
+          [
+            %Embed{}
+            |> Embed.put_title(md_escape(info["name"]))
+            |> Embed.put_url(info["scryfall_uri"])
+            |> Embed.put_description(
+              Emoji.emojify(info["mana_cost"] <> "\n" <> format_description(info))
+            )
+            |> put_legalities(info["legalities"])
+            |> Embed.put_image(info["image_uris"]["art_crop"]),
+            %Embed{}
+            |> put_rulings(rulings)
+            |> Embed.put_footer(
+              case Enum.random(1..5000) do
+                x when x in 1..4999 -> "data sourced from Scryfall"
+                5000 -> "data forcefully ripped from the cold, dead hands of Scryfall"
+              end,
+              "https://cdn.discordapp.com/app-icons/268547439714238465/f13c4408ead703ef3940bc7e21b91e2b.png"
+            )
+          ]
+      end
+
+    for embed <- embeds do
+      Api.create_message(ctx.channel_id, embed: embed)
+    end
+  end
+
+  defp return_alternate_cards({:error, message}, _, ctx), do: return_error(message, ctx)
+
+  defp return_alternate_cards({:ok, resp}, card_name, ctx) do
+    card_list =
+      resp.body["data"]
+      |> Enum.map(fn x -> "- #{x}" end)
+      |> Enum.join("\n")
+
+    embed =
+      %Embed{}
+      |> Embed.put_color(Colors.warning())
+      |> Embed.put_title("Ambiguous card name")
+      |> Embed.put_description("""
+      The name \"#{card_name}\" matches too many cards.
+      Did you mean one of these?
+
+      #{card_list}
+      """)
+
+    Api.create_message(ctx.channel_id, embed: embed)
+  end
+
+  defp return_error(message, ctx) do
+    Api.create_message(ctx.channel_id, embed: message)
+  end
+
+  defp get_alternate_cards(card_name) do
+    Scrybot.Scryfall.Api.autocomplete(card_name)
+  end
+
+  defp get_exact_card_info(card_name) do
+    Scrybot.Scryfall.Api.cards_named(card_name, true)
+  end
+
   defp get_card_info(card_name) do
     Scrybot.Scryfall.Api.cards_named(card_name, false)
   end
@@ -202,10 +310,5 @@ defmodule Scrybot.Discord.Command.CardInfo do
   defp notify_error(reason, channel) do
     Logger.error(inspect(reason))
     Nostrum.Api.create_message(channel, embed: reason)
-  end
-
-  defp notify_error!(reason, channel) do
-    Logger.error(inspect(reason))
-    Nostrum.Api.create_message!(channel, embed: reason)
   end
 end
