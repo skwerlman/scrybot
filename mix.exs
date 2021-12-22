75626f6af5dceccf7268c4da183f761815ef374f
skwerlman <skwerlman@users.noreply.github.com>
2021-12-21 21:18:57 -0500

    bump ver

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Scrybot.MixProject do
   def project do
     [
       app: :scrybot,
-      version: "0.14.7",
+      version: "0.15.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
